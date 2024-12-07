1ca434c648d3b5510b6433558654e7994fdfe4de
NoahRaigrodski <nraigrodski@gmail.com>
2024-12-07 00:45:59 +0000

    commit -m “Submition”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,3 +1,4 @@
+
 //
 //  Class author:  Noah Raigrodski
 //  Date created:  12/4/24
@@ -6,76 +7,75 @@
 import java.awt.*;
 
 public class Ball {
-	
+
 	private int x;
 	private int y;
 	private int xVelocity;
 	private int yVelocity;
 	private int size;
-	
-	
-	//constructor(s):
-	public Ball(int x, int y, int size){
+
+	// constructor(s):
+	public Ball(int x, int y, int size) {
 		this.x = x;
 		this.y = y;
 		this.size = size;
 		xVelocity = 1;
 		yVelocity = -2;
 	}
-	
-	//methods:
-	//Accessor methods
-	public int getXpos(){
+
+	// methods:
+	// Accessor methods
+	public int getXpos() {
 		return x;
 	}
 
-	public int getYpos(){
+	public int getYpos() {
 		return y;
 	}
 
-	public int getSize(){
+	public int getSize() {
 		return size;
 	}
 
-	//Mutator methods
-	public void setX(int x){
+	// Mutator methods
+	public void setX(int x) {
 		this.x = x;
 	}
 
-	public void setY(int y){
+	public void setY(int y) {
 		this.y = y;
 	}
 
-	public void setXVelocity(int velocity){
+	public void setXVelocity(int velocity) {
 		xVelocity = velocity;
 	}
 
-	public void setYVelocity(int velocity){
+	public void setYVelocity(int velocity) {
 		yVelocity = velocity;
 	}
 
-	public void reverseX(){
+	public void reverseX() {
 		xVelocity = -xVelocity;
 	}
 
-	public void reverseY(){
+	public void reverseY() {
 		yVelocity = -yVelocity;
 	}
 
 	//
-	//  Pre-condition: g must have a value
-	//  Post-condition: a graphics object will be drawn
+	// Pre-condition: g must have a value
+	// Post-condition: a graphics object will be drawn
 	//
-	public void draw(Graphics g){
+	public void draw(Graphics g) {
 		g.setColor(Color.white);
-        g.fillOval(x, y, size,size);
+		g.fillOval(x, y, size, size);
 	}
 
 	//
-	//  Pre-condition: An instance of this class must exist
-	//  Post-condition: The ball's x and y posistions will be updated
+	// Pre-condition: An instance of this class must exist
+	// Post-condition: The ball's x and y posistions will be updated
 	//
-	public void move(){
+	public void move() {
 		x += xVelocity;
 		y += yVelocity;
 	}
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,3 +1,4 @@
+
 //
 //  Class author:  Noah Raigrodski
 //  Date created:  12/4/24
@@ -6,85 +7,83 @@
 import java.awt.*;
 
 public class Paddle {
-	
+
 	private int x;
 	private int y;
 	private int width;
 	private int height;
 	private int velocity;
-	
-	
-	//constructor(s):
-	public Paddle(int x, int y, int width, int height){
+
+	// constructor(s):
+	public Paddle(int x, int y, int width, int height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		velocity = 0;
 	}
-	
-	//methods:
 
-	//Accessor methods
-	public int getX(){
+	// methods:
+
+	// Accessor methods
+	public int getX() {
 		return x;
 	}
 
-	public int getY(){
+	public int getY() {
 		return y;
 	}
 
-	public int getWidth(){
+	public int getWidth() {
 		return width;
 	}
 
-	public int getHeight(){
+	public int getHeight() {
 		return height;
 	}
 
-	public int getVelocity(){
+	public int getVelocity() {
 		return velocity;
 	}
 
-	//Mutator methods
-	public void setX(int x){
+	// Mutator methods
+	public void setX(int x) {
 		this.x = x;
 	}
 
-	public void setVelocity(int velocity){
+	public void setVelocity(int velocity) {
 		this.velocity = velocity;
 	}
 
 	//
-	//  Pre-condition: The argument given must be an int
-	//  Post-condition: Velocity is updated to add the bonus velocity
+	// Pre-condition: The argument given must be an int
+	// Post-condition: Velocity is updated to add the bonus velocity
 	//
-	public void addVelocity(int bonusVelocity){
+	public void addVelocity(int bonusVelocity) {
 		velocity += bonusVelocity;
 	}
 
 	//
-	//  Pre-condition: An instance of this class must exist
-	//  Post-condition: The Paddle's x posistion will be updated
+	// Pre-condition: An instance of this class must exist
+	// Post-condition: The Paddle's x posistion will be updated
 	//
-	public void move(){
-		x+= velocity;
+	public void move() {
+		x += velocity;
 
 		if (velocity > 0) {
-			velocity--; 
+			velocity--;
 		} else if (velocity < 0) {
 			velocity++;
 		}
 	}
 
 	//
-	//  Pre-condition: g must have a value
-	//  Post-condition: a graphics object will be drawn
+	// Pre-condition: g must have a value
+	// Post-condition: a graphics object will be drawn
 	//
-	public void draw(Graphics g){
+	public void draw(Graphics g) {
 		g.setColor(Color.blue);
-        g.fillRect(x, y, width, height);
+		g.fillRect(x, y, width, height);
 	}
-	
-	
+
 }

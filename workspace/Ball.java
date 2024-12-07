d44fbcf5eec66e5196d8b1c6702193380bec667e
NoahRaigrodski <nraigrodski@gmail.com>
2024-12-07 00:44:22 +0000

    commit -m “Submition 12/6”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,14 +1,83 @@
+//
+//  Class author:  Noah Raigrodski
+//  Date created:  12/4/24
+//  General description: This class blueprints a ball object which moves and appears on the screen
+//
 import java.awt.*;
 
 public class Ball {
-	//your code here!
 	
-	//don't forget you need instance variables:
-	
+	private int x;
+	private int y;
+	private int xVelocity;
+	private int yVelocity;
+	private int size;
 	
 	
 	//constructor(s):
+	public Ball(int x, int y, int size){
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		xVelocity = 1;
+		yVelocity = -2;
+	}
 	
 	//methods:
+	//Accessor methods
+	public int getXpos(){
+		return x;
+	}
+
+	public int getYpos(){
+		return y;
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	//Mutator methods
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public void setY(int y){
+		this.y = y;
+	}
+
+	public void setXVelocity(int velocity){
+		xVelocity = velocity;
+	}
+
+	public void setYVelocity(int velocity){
+		yVelocity = velocity;
+	}
+
+	public void reverseX(){
+		xVelocity = -xVelocity;
+	}
+
+	public void reverseY(){
+		yVelocity = -yVelocity;
+	}
+
+	//
+	//  Pre-condition: g must have a value
+	//  Post-condition: a graphics object will be drawn
+	//
+	public void draw(Graphics g){
+		g.setColor(Color.white);
+        g.fillOval(x, y, size,size);
+	}
+
+	//
+	//  Pre-condition: An instance of this class must exist
+	//  Post-condition: The ball's x and y posistions will be updated
+	//
+	public void move(){
+		x += xVelocity;
+		y += yVelocity;
+	}
 
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,19 +1,90 @@
-import java.awt.Graphics;
+//
+//  Class author:  Noah Raigrodski
+//  Date created:  12/4/24
+//  General description: This class blueprints a paddle object which appears on the screen and moves
+//
 import java.awt.*;
 
 public class Paddle {
-
-
-	//your code here!
-
-
 	
-	//don't forget you need instance variables:
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int velocity;
 	
 	
 	//constructor(s):
+	public Paddle(int x, int y, int width, int height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		velocity = 0;
+	}
 	
 	//methods:
+
+	//Accessor methods
+	public int getX(){
+		return x;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
+	public int getVelocity(){
+		return velocity;
+	}
+
+	//Mutator methods
+	public void setX(int x){
+		this.x = x;
+	}
+
+	public void setVelocity(int velocity){
+		this.velocity = velocity;
+	}
+
+	//
+	//  Pre-condition: The argument given must be an int
+	//  Post-condition: Velocity is updated to add the bonus velocity
+	//
+	public void addVelocity(int bonusVelocity){
+		velocity += bonusVelocity;
+	}
+
+	//
+	//  Pre-condition: An instance of this class must exist
+	//  Post-condition: The Paddle's x posistion will be updated
+	//
+	public void move(){
+		x+= velocity;
+
+		if (velocity > 0) {
+			velocity--; 
+		} else if (velocity < 0) {
+			velocity++;
+		}
+	}
+
+	//
+	//  Pre-condition: g must have a value
+	//  Post-condition: a graphics object will be drawn
+	//
+	public void draw(Graphics g){
+		g.setColor(Color.blue);
+        g.fillRect(x, y, width, height);
+	}
 	
 	
 }
